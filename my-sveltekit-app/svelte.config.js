511294dfbce4b4b581b3b57d10b48130d37122f8
Harsh Sharma <harshsharma90866@gmail.com>
2025-05-14 07:06:09 +0530

    add vercel.json

--- my-sveltekit-app/svelte.config.js
+++ my-sveltekit-app/svelte.config.js
@@ -1,5 +1,17 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-const config = { kit: { adapter: adapter() } };
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            // Add Vercel Edge Functions and File System API support
+            runtime: 'edge',
+            regions: ['fra1']
+        }),
+        files: {
+            assets: 'static'
+        }
+    }
+};
 
 export default config;
